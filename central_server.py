ae2bf4dcdec4dd88c7450874f57d458ab98d5818
Nguyen Duc An <an.nguyenduc1406@hcmut.edu.vn>
2022-11-26 10:40:26 +0700

    update deploy

--- central_server.py
+++ central_server.py
@@ -2,6 +2,7 @@ import threading
 from base import Base
 from persistence import *
 
+
 class CentralServer(Base):
     def __init__(self, serverhost='localhost', serverport=40000):
         super(CentralServer, self).__init__(serverhost, serverport)
@@ -11,7 +12,7 @@ class CentralServer(Base):
 
         # manage online user list
         self.onlinelist = {}
-        
+
         # define handlers for received message of central server
         handlers = {
             'PEER_REGISTER': self.peer_register,
@@ -20,8 +21,8 @@ class CentralServer(Base):
         }
         for msgtype, function in handlers.items():
             self.add_handler(msgtype, function)
-    
-    ##==========implement protocol for user registration - central server==========##
+
+    ## ==========implement protocol for user registration - central server==========##
     def peer_register(self, msgdata):
         # received register info (msgdata): peername, host, port, pass (hashed)
         peer_name = msgdata['peername']
@@ -31,18 +32,20 @@ class CentralServer(Base):
         # register error if peer has been connected with central server
         # otherwise add peer to managed user list of central server
         if peer_name in self.peerlist:
-            self.client_send((peer_host, peer_port), msgtype='REGISTER_ERROR', msgdata={})
-            print (peer_name," has been refused for registration!")
+            self.client_send((peer_host, peer_port),
+                             msgtype='REGISTER_ERROR', msgdata={})
+            print(peer_name, " has been refused for registration!")
         else:
             # add peer to managed user list
             self.peerlist.append(peer_name)
             # save to database
-            add_new_user(peer_name,peer_password)
-            self.client_send((peer_host, peer_port), msgtype='REGISTER_SUCCESS', msgdata={})
-            print (peer_name," has been added to central server's managed user list!")
-    ##===========================================================##
+            add_new_user(peer_name, peer_password)
+            self.client_send((peer_host, peer_port),
+                             msgtype='REGISTER_SUCCESS', msgdata={})
+            print(peer_name, " has been added to central server's managed user list!")
+    ## ===========================================================##
 
-    ##==========implement protocol for authentication (log in) - central server==========##
+    ## ==========implement protocol for authentication (log in) - central server==========##
     def peer_login(self, msgdata):
         # received login info (msgdata): peername, host, port, pass (hashed)
         peer_name = msgdata['peername']
@@ -57,33 +60,36 @@ class CentralServer(Base):
             if str(peer_password) == peer_pass_retrieved:
                 # add peer to online user list
                 self.onlinelist[peer_name] = tuple((peer_host, peer_port))
-                self.client_send((peer_host, peer_port), msgtype='LOGIN_SUCCESS', msgdata={})
-                print (peer_name," has been added to central server's online user list!")
+                self.client_send((peer_host, peer_port),
+                                 msgtype='LOGIN_SUCCESS', msgdata={})
+                print(peer_name, " has been added to central server's online user list!")
             else:
-                self.client_send((peer_host, peer_port), msgtype='LOGIN_ERROR', msgdata={})
-                print (peer_name," has been refused for login!")
+                self.client_send((peer_host, peer_port),
+                                 msgtype='LOGIN_ERROR', msgdata={})
+                print(peer_name, " has been refused for login!")
         else:
-            self.client_send((peer_host, peer_port), msgtype='LOGIN_ERROR', msgdata={})
-            print (peer_name," has been refused for login!")
-    ##===========================================================##
+            self.client_send((peer_host, peer_port),
+                             msgtype='LOGIN_ERROR', msgdata={})
+            print(peer_name, " has been refused for login!")
+    ## ===========================================================##
 
-    ##==========implement protocol for getting online user list - central server==========##
-    def peer_list(self, msgdata): 
+    ## ==========implement protocol for getting online user list - central server==========##
+    def peer_list(self, msgdata):
         peer_name = msgdata['peername']
         peer_host = msgdata['host']
         peer_port = msgdata['port']
         data = {'online_user_list': self.onlinelist}
-        self.client_send((peer_host, peer_port), msgtype='LIST_ONLINE_USER', msgdata=data)
-        print (peer_name," has been sent latest online user list!")
-    ##===========================================================##
+        self.client_send((peer_host, peer_port),
+                         msgtype='LIST_ONLINE_USER', msgdata=data)
+        print(peer_name, " has been sent latest online user list!")
+    ## ===========================================================##
 
     def run(self):
         # A child thread for receiving message
         t = threading.Thread(target=self.recv)
         t.start()
 
+
 if __name__ == '__main__':
     server = CentralServer()
     server.run()
-
-
